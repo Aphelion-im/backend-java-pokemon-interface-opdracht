81ad06a25edb481bbd91a34fb08e6a96244e36dc
Aphelion-im <aphelion-im@protonmail.com>
2023-09-22 22:42:35 +0200

    Finished assignment

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+// https://bulbagarden.net
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
---
+++ src/Pokemon.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+public abstract class Pokemon {
+
+    private final String name; // Moest final
+    private final int level; // Moest final
+    private int hp;
+    private final String food; // Moest final
+    private final String sound; // Moest final
+    private final String type; // Moest final
+
+    public Pokemon(String name, int level, int hp, String food, String sound, String type) {
+        this.name = name;
+        this.level = level;
+        this.hp = hp;
+        this.food = food;
+        this.sound = sound;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
